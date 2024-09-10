19f98843774e892527383379b81f51fb46318db2
abhinashgupta <abhinahgupta1822@gmail.com>
2024-09-10 23:45:12 +0530

    todo completed with update functionality

--- routes/index.js
+++ routes/index.js
@@ -8,24 +8,42 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/todo', async (req, res) => {
-  const todos = await UserTodo.find();
-  res.render('todo' , {todos});
-})
+    const todos = await UserTodo.find();
+    res.render('todo' , {todos});
+});
 
 router.post("/createTodo", async (req, res) => {
-  const { name , email , description , status} = req.body;
-  const todo = await UserTodo.create({
-    name,
-    email,
-    description,
-    status
-  });
-  res.redirect('/todo');
+  const { name, email, description, status, userId } = req.body;
+  if (userId) {
+    await UserTodo.findByIdAndUpdate(
+      { _id: userId },
+      { name, email, description, status }
+    );
+    res.redirect('/todo');
+  } else {
+    const todo = await UserTodo.create({
+      name,
+      email,
+      description,
+      status,
+    });
+    res.redirect("/todo");
+  }
+  
 });
 
 router.get('/deleteTodo/:id', async (req, res) => {
   const todo = await UserTodo.findByIdAndDelete({ _id: req.params.id });
   res.redirect('/todo');
 });
+
+router.post('/updateTodo/:id', async (req, res) => {
+  const {name , email , description , status } = req.body;
+  const todo = await UserTodo.findByIdAndUpdate(
+    { _id: req.params.id },
+    { name, email, description, status }
+  );
+  res.redirect('/todo');
+})
 
 module.exports = router;
